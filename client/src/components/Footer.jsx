39b76ecfecf37cee36ba5d22ebe14e3eb2902128
KARTAVYA PANTHI <97825212+Kartv07@users.noreply.github.com>
2023-02-16 20:31:19 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
                    <div className="join p-5"><p className='text-4xl text-white p-3 '>Join DeFin</p><br /></div>
                     <div className='flex flex-col'>
                         <div className="flex flex-row">
-                        <a href="https://github.com/Kartv07/DeFin-Ethereum" alt="#" target="_blank" className='link cursor-pointer'><img src={github}  alt="logo" className="w-20 mx-5 cursor-pointer"/></a>
+                        <a href="https://github.com/Kartv07/Defin" alt="#" target="_blank" className='link cursor-pointer'><img src={github}  alt="logo" className="w-20 mx-5 cursor-pointer"/></a>
                         <a href="https://www.linkedin.com/in/kartavya-panthi/" alt="#" target="_blank"  className='link cursor-pointer'><img src={linkedin}  alt="logo" className="w-20  mx-5 cursor-pointer"/></a>
                         </div>
                     </div>
